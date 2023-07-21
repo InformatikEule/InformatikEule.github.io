e4bd2d920ee5d174a580c88bc67d9ce42ad4ac84
Eule <felixweisselberg@web.de>
2023-07-21 20:50:23 +0200

    changed time display on my super secret page to 24hours format

--- js/myLittleSecret.js
+++ js/myLittleSecret.js
@@ -1,48 +1,11 @@
-const AtlanticTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Atlantic",
-});
-
-const CentralTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Central",
-});
-
-const EasternTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Eastern",
-});
-
-const MountainTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Mountain",
-});
-
-const NewfoundlandTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Newfoundland",
-});
-
-const PacificTime = new Date().toLocaleString("en-US", {
+const PacificTime = new Date().toLocaleString("en-GB", {
   timeZone: "Canada/Pacific",
 });
 
-const SaskatchewanTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Saskatchewan",
-});
-
-const YukonTime = new Date().toLocaleString("en-US", {
-  timeZone: "Canada/Yukon",
-});
-
-const berlinTime = new Date().toLocaleString("en-US", {
+const berlinTime = new Date().toLocaleString("en-GB", {
   timeZone: "Europe/Berlin",
 });
-console.log("Central: " + CentralTime);
-console.log("Eastern: " + EasternTime);
-console.log("Mountain: " + MountainTime);
-console.log("Newfoundland: " + NewfoundlandTime);
-console.log("Pacific: " + PacificTime);
-console.log("Saskatchewan: " + SaskatchewanTime);
-console.log("Yukon: " + YukonTime);
 
-//console.log(`The current time in Houston is: ${houstonTime}`);
-console.log(`The current time in Berlin is: ${berlinTime}`);
 document.getElementById("berlinTime").textContent = "Leipzig: " + berlinTime;
 document.getElementById("vancouverTime").textContent =
   "Vancouver/Williams Lake: " + PacificTime;
