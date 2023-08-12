a67d73c3e5e3259d8e65ef56030a90cb12717ccc
Eule <felixweisselberg@web.de>
2023-08-12 13:18:47 +0200

    changed alot / added new stuff / working on contact me page

--- js/contactMe.js
+++ js/contactMe.js
@@ -1,24 +1,25 @@
-function sendAMailToMe() {
-  var dataMail = {
-    mailFrom: document.getElementById("mailFrom").value,
-    mailAddress: document.getElementById("mailAddress").value,
-    mailMessage: document.getElementById("mailMessage").value,
-  };
+// function sendAMailToMe() {
 
-  const serviceId = "service_j59bd2x";
-  const templateId = "template_301y19v";
+//   var dataMail = {
+//     mailFrom: document.getElementById("mailFrom").value,
+//     mailAddress: document.getElementById("mailAddress").value,
+//     mailMessage: document.getElementById("mailMessage").value,
+//   };
 
-  emailjs
-    .send(serviceId, templateId, dataMail)
-    .then((resp) => {
-      document.getElementById("mailFrom").value = "";
-      document.getElementById("mailAddress").value = "";
-      document.getElementById("mailMessage").value = "";
-      console.log(resp);
-      alert("You send a Mail to me!");
-    })
-    .catch((errorMail) => alert("Error: " + errorMail));
-}
+//   const serviceId = "service_j59bd2x";
+//   const templateId = "template_301y19v";
+
+//   emailjs
+//     .send(serviceId, templateId, dataMail)
+//     .then((resp) => {
+//       document.getElementById("mailFrom").value = "";
+//       document.getElementById("mailAddress").value = "";
+//       document.getElementById("mailMessage").value = "";
+//       console.log(resp);
+//       alert("You send a Mail to me!");
+//     })
+//     .catch((errorMail) => alert("Error: " + errorMail));
+// }
 
 function notWorking() {
   alert("this function is not working atm.");
