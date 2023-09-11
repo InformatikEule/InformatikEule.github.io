6c58c7152a415e5aec447314282a8c1046094d77
Eule <felixweisselberg@web.de>
2023-09-11 22:54:23 +0200

    rover page displays the pictures now as cards

--- js/rover.js
+++ js/rover.js
@@ -7,27 +7,6 @@ searchBRov.addEventListener("click", () => {
   reqRov();
 });
 
-function rovCam() {
-  //sol 2553 (12.11.2019)auf cam "MAHLI" lohnt! (panoramaaufnahme)
-  //sol 0 cam MARDI  (landing!)!
-  //sol 1512 (06.11.2016) MAHLI
-  return "MAHLI";
-  // if (document.getElementById("fhaz").checked) {
-  //   //console.log("Fhaz");
-  //   return "FHAZ";
-  // } else if (document.getElementById("rhaz").checked) {
-  //   //console.log("Rhaz");
-  //   return "RHAZ";
-  // } else {
-  //   alert("cmon, click a button....");
-  // }
-}
-
-// function getSol() {
-//   var sol = document.getElementById("sol").value;
-//   return sol;
-// }
-
 function getEarthDate() {
   var dateToday = new Date();
   var dateFormatted = dateToday.toISOString().slice(0, 10);
@@ -60,10 +39,9 @@ function getEarthDate() {
 async function reqRov() {
   //const sol = getSol();
   const earthDate = getEarthDate();
-  const cam = rovCam();
 
   let respRov = await fetch(
-    `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earthDate}&api_key=DEMO_KEY&cam=${cam}`
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earthDate}&api_key=DEMO_KEY`
   );
   if (respRov.status >= 200 && respRov.status < 400) {
     let dataRov = await respRov.json();
@@ -78,21 +56,30 @@ async function reqRov() {
 
 function useDataRov(dataRov) {
   const display = document.querySelector("#marsRoverDisplay");
+  const displayDescriptive = document.querySelector(
+    "#marsRoverDescriptiveDisplay"
+  );
+
+  let roverData = dataRov.photos.rover[0];
+  (displayDescriptive.innerHTML = "Rover Name: " + roverData.name),
+    ". Landing Date: " + roverData.landing_date,
+    ". Last day with available Photos: " + roverData.max_date;
+
   let showData = dataRov.photos
     .map((data) => {
-      const { img_src, rover } = data;
+      const { img_src, rover, camera } = data;
       //console.log(img_src);
-      return `
-      <p class="text-center text-white">Rover: ${rover.name} Landing Date: ${rover.landing_date} Mission End date: ${rover.max_date}</p> 
-      <div class="row">
-        <div class="col-4">
-          <ul class="list-group">
-            <li class="list-group-item flex-fill">
-              <img src="${img_src}" class="img-fluid"></img>
-            </li>
-          </ul>
+      return `<div class="col-sm-3">
+      <div class="card bg-dark border-secondary">
+
+        <img src="${img_src}" class="card-img-top"></img>
+
+        <div class="card-body">
+          <p class="text-center text-white">Camera: ${camera.name}</p>
         </div>
       </div>
+      </div>
+      <!---->
       `;
     })
     .join("");
