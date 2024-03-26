499743e0ad635cbfbd14c28974808ed3ced3d136
Eule <felixweisselberg@web.de>
2024-03-26 19:44:38 +0100

    first steps for my new front page. added a section for upcoming rocket launches and begin using the launchlibraryAPI. adjustet html css and js!

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
-const themesBtn = document.querySelector("#themesBtn");
+////
+// rocketLaunches.js
+////
 
-themesBtn.addEventListener("Click", () => {
-  alert("hi");
-  toggleTheme();
-});
+// function getCity() {
+//   const cityName = document.getElementById("getCityName").value;
+//   return cityName;
+// }
+// async function upcomingLaunches() {
+//   //const city = getCity();
+//   let resp = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/upcoming/`);
+//   //prüfen ob der server einen fehler meldet:
+//   if (resp.status >= 200 && resp.status < 400) {
+//     let launchData = await resp.json();
+//     useLaunchData(launchData);
+//   } else {
+//     console.log("Error, Status code: " + resp.status);
+//     alert(
+//       "Error, Server returns status-code: " +
+//         resp.status +
+//         "! Status-Text: " +
+//         resp.statusText
+//     );
+//   }
+// }
 
-function toggleTheme() {
-  // Obtains an array of all <link>
-  // elements.
-  // Select your element using indexing.
-  alert("Hallo");
-  // var theme = document.getElementsByTagName("link")[0];
-
-  // // Change the value of href attribute
-  // // to change the css sheet.
-  // if (theme.getAttribute("href") == "styles.css") {
-  //   theme.setAttribute("href", "stylesDark.css");
-  // } else {
-  //   theme.setAttribute("href", "styles.css");
-  // }
+function useLaunchData(launchData) {
+  console.log(launchData.results[1].name);
+  console.log(launchData.results[1].location.name);
+  console.log(launchData.results[1].pad.name);
+  console.log(launchData.results[1].image);
+  const nextLaunch = document.getElementById("nextLaunch");
+  nextLaunch.textContent = launchData;
 }
