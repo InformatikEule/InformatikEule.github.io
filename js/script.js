3c11b56895118536b41521e84dc658093917b105
Eule <felixweisselberg@web.de>
2024-03-29 18:44:18 +0100

    implemented a skeleton loading animation for better experience with slow internet speed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 ////
 // rocketLaunches.js
 ////
-
-// function getCity() {
-//   const cityName = document.getElementById("getCityName").value;
-//   return cityName;
-// }
 async function upcomingLaunches() {
-  //const city = getCity();
   let resp = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/upcoming/`);
   //prüfen ob der server einen fehler meldet:
   if (resp.status >= 200 && resp.status < 400) {
     let launchData = await resp.json();
     useLaunchData(launchData);
+    //prüfen ob die 15 reqs/day abgelaufen sind
   } else if (resp.status == 429) {
+    //error handling ändern. gib die original felder aus useLaunchData zurück und pack die fehlermeldung dort rein!
     alert("Too many requests.");
   } else {
     console.log("Error, Status code: " + resp.status);
@@ -24,20 +20,19 @@ async function upcomingLaunches() {
         resp.statusText
     );
   }
-
-  // if (resp.status >= 200 && resp.status < 400) {
-  //   let launchData = await resp.json();
-  //   useLaunchData(launchData);
-  // } else {
-  //   console.log("Error, Status code: " + resp.status);
-  //   alert(
-  //     "Error, Server returns status-code: " +
-  //       resp.status +
-  //       "! Status-Text: " +
-  //       resp.statusText
-  //   );
-  // }
 }
+// if (resp.status >= 200 && resp.status < 400) {
+//   let launchData = await resp.json();
+//   useLaunchData(launchData);
+// } else {
+//   console.log("Error, Status code: " + resp.status);
+//   alert(
+//     "Error, Server returns status-code: " +
+//       resp.status +
+//       "! Status-Text: " +
+//       resp.statusText
+//   );
+// }
 
 function useLaunchData(launchData) {
   console.log(launchData.results[1].name);
@@ -46,7 +41,9 @@ function useLaunchData(launchData) {
   const launchImg = document.getElementById("launchImg");
   launchImg.src = launchData.results[1].image;
   const rocketType = document.getElementById("rocketType");
-  rocketType.textContent = launchData.results[1].name;
+  rocketType.innerHTML = "";
+  rocketType.innerHTML = launchData.results[1].name;
   const spacePort = document.getElementById("spacePort");
+  spacePort.innerHTML = "";
   spacePort.textContent = launchData.results[1].pad.name;
 }
