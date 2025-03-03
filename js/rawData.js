9dc9b700390663d8de44ac10e86956a14e3c1c9b
InformatikEule <felixweisselberg@web.de>
2025-03-04 00:27:59 +0100

    styling the rawData Page and working on the JS code

---
+++ js/rawData.js
@@ -0,0 +1,9 @@
+const btnRawData = document.getElementById("getRawData");
+
+btnRawData.addEventListener("click", () => {
+  getRawData();
+});
+
+function getRawData() {
+  console.log("Hello World");
+}
