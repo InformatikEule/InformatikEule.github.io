26148ff2cabb0524bc60751c2e9b6db78ab57db0
InformatikEule <felixweisselberg@web.de>
2025-03-04 01:19:52 +0100

    working on the js file

--- js/rawData.js
+++ js/rawData.js
@@ -4,6 +4,18 @@ btnRawData.addEventListener("click", () => {
   getRawData();
 });
 
-function getRawData() {
-  console.log("Hello World");
+async function getRawData() {
+  let data = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/upcoming/`);
+  if (!data.ok) {
+    alert("Sum ting wroang!");
+  } else {
+    let dataRaw = await data.json();
+    console.log(dataRaw);
+    useRawData(dataRaw);
+  }
+}
+
+function useRawData(dataRaw) {
+  //komme hier grad nicht weiter sadface.jpg
+  console.log("hier" + dataRaw.results[0]);
 }
