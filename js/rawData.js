51c93c6a0f7f4e41be26514ae642aff71f6f892d
InformatikEule <felixweisselberg@web.de>
2025-03-05 21:59:48 +0100

    got it! page works and shows the first 10 names

--- js/rawData.js
+++ js/rawData.js
@@ -11,24 +11,9 @@ async function getRawData() {
   } else {
     let dataRaw = await data.json();
     console.log(dataRaw);
-    //useRawData(dataRaw);
     const dataRawList = document.querySelector("ul");
     dataRaw.results.forEach((element) => {
-      console.log(element);
-      // dataRawList.insertAdjacentElement(
-      //   "beforeend",
-      //   "<li>${element.name}</li>"
-      // );
+      dataRawList.insertAdjacentHTML(`beforeend`, `<li>${element.name}</li>`);
     });
   }
 }
-
-//function useRawData(dataRaw) {
-
-//zeigt den namen der ersten rakete...
-//console.log(dataRaw.results[0].name);
-//dataRaw.forEach((element) => {
-//console.log("ji");
-//dataRawList.insertAdjacentElement("beforeend", `<li>${element.name}</li>`);
-//});
-//}
