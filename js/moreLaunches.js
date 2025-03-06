50bc0bf74db69a0e8dd21fd10504441a4c07ad39
InformatikEule <felixweisselberg@web.de>
2025-03-07 00:27:00 +0100

    renamed few files. moreLaunches page dosnt work with buttonclick anymore but has a bodyOnLoad func

--- js/moreLaunches.js
+++ js/moreLaunches.js
@@ -1,10 +1,4 @@
-const btnRawData = document.getElementById("getRawData");
-
-btnRawData.addEventListener("click", () => {
-  getRawData();
-});
-
-async function getRawData() {
+async function getMoreLaunches() {
   let data = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/upcoming/`);
   if (!data.ok) {
     alert("Sum ting wroang!");
