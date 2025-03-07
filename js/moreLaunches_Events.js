2d760b020e16b0c29fa4691b13812db31998a5e9
InformatikEule <felixweisselberg@web.de>
2025-03-07 15:20:18 +0100

    more events page is now working. better styling for the buttons that link to those sites

--- js/moreLaunches_Events.js
+++ js/moreLaunches_Events.js
@@ -24,25 +24,24 @@ async function getMoreLaunches() {
 
 //function um die "more Events" page mit Daten zu füllen
 async function getMoreEvents() {
-  alert("bald!");
-  //   let data = await fetch(`https://ll.thespacedevs.com/2.2.0/launch/upcoming/`);
-  //   if (!data.ok) {
-  //     alert("Sum ting wroang!");
-  //   } else {
-  //     let dataRaw = await data.json();
-  //     console.log(dataRaw);
-  //     const dataRawList = document.querySelector("ul");
-  //     dataRaw.results.forEach((element) => {
-  //       dataRawList.insertAdjacentHTML(
-  //         `beforeend`,
-  //         `<li>${element.name}<ul><img class="img-fluid launchImg" src="${
-  //           element.image
-  //         }"></img><li>${element.net.slice(0, 10)}<li>${element.net.slice(
-  //           11,
-  //           19
-  //         )}</li></li><li>${element.mission.description}</li></ul></li><br>`
-  //         //`<li>${element.name}<ul><img class="img-fluid launchImg" src="${element.image}"></img></ul><ul>${element.net}</ul><ul>${element.mission.description}</ul></li>`
-  //       );
-  //     });
-  //   }
+  let data = await fetch(`https://ll.thespacedevs.com/2.2.0/event/upcoming/`);
+  if (!data.ok) {
+    alert("Sum ting wroang!");
+  } else {
+    let dataRaw = await data.json();
+    console.log(dataRaw);
+    const dataRawList = document.querySelector("ul");
+    dataRaw.results.forEach((element) => {
+      dataRawList.insertAdjacentHTML(
+        `beforeend`,
+        `<li>${element.name}<ul><img class="img-fluid launchImg" src="${
+          element.feature_image
+        }"></img><li>${element.date.slice(0, 10)}<li>${element.date.slice(
+          11,
+          19
+        )}</li></li><li>${element.description}</li></ul></li><br>`
+        //`<li>${element.name}<ul><img class="img-fluid launchImg" src="${element.image}"></img></ul><ul>${element.net}</ul><ul>${element.mission.description}</ul></li>`
+      );
+    });
+  }
 }
