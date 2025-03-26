6f65720544b28d705bd7366a1387df811e64ab2d
InformatikEule <felixweisselberg@web.de>
2025-03-26 20:12:03 +0100

    removed unused code

--- js/myLittleSecret.js
+++ js/myLittleSecret.js
@@ -1,48 +1,17 @@
-// const pacificTime = new Date().toLocaleString("en-GB", {
-//   timeZone: "Canada/Pacific",
-// });
+const pacificTime = new Date().toLocaleString("en-GB", {
+  timeZone: "Canada/Pacific",
+});
 
-// const mountainTime = new Date().toLocaleString("en-GB", {
-//   timeZone: "America/Edmonton",
-// });
+const mountainTime = new Date().toLocaleString("en-GB", {
+  timeZone: "America/Edmonton",
+});
 
-// const berlinTime = new Date().toLocaleString("en-GB", {
-//   timeZone: "Europe/Berlin",
-// });
+const berlinTime = new Date().toLocaleString("en-GB", {
+  timeZone: "Europe/Berlin",
+});
 
-// document.getElementById("berlinTime").textContent = "Leipzig: " + berlinTime;
-// document.getElementById("vancouverTime").textContent =
-//   "Vancouver/Williams Lake: " + pacificTime;
-// document.getElementById("calgaryTime").textContent =
-//   "Calgary/Cranbrooke: " + mountainTime;
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   //console.log("jo");
-//   loadCSV("Data/data.csv");
-// });
-
-async function loadCSV(filename) {
-  const response = await fetch(filename);
-  if (!response.ok) {
-    throw new Error(`Fehler beim Laden der Datei: ${response.statusText}`);
-  }
-  const text = await response.text();
-  const data = parseCSV(text);
-  console.log(data);
-  const abbreviationsPage = document.querySelector("ul");
-  data.map((element) => {
-    const withOutCommas = element.join(" -> ");
-    abbreviationsPage.insertAdjacentHTML(
-      `beforeend`,
-      `<li class="text-white">${withOutCommas}</li>`
-    );
-  });
-}
-
-function onLoad() {
-  loadCSV("Data/data.csv");
-}
-
-function parseCSV(text) {
-  return text.split("\n").map((row) => row.split(";"));
-}
+document.getElementById("berlinTime").textContent = "Leipzig: " + berlinTime;
+document.getElementById("vancouverTime").textContent =
+  "Vancouver/Williams Lake: " + pacificTime;
+document.getElementById("calgaryTime").textContent =
+  "Calgary/Cranbrooke: " + mountainTime;
